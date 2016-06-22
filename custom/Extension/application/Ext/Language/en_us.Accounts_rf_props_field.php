65a8e8e88751f78d96eeb68ad65e51070fd20e76
Евгений Первушин <pea@lab321.ru>
2016-06-22 10:48:12 +0600

    Accounts_rf_props_field-1.0.0

--- custom/Extension/application/Ext/Language/en_us.Accounts_rf_props_field.php
+++ custom/Extension/application/Ext/Language/en_us.Accounts_rf_props_field.php
@@ -4,6 +4,6 @@ $app_list_strings['resident_list'] = array(
     'rf' => 'RF',
     'not_rf' => 'Not RF',
 );
-$app_list_strings['org_form_dom']= get_dom_list('ru_ru','org_form_dom',true);
+$app_list_strings['org_form_dom']= function_exists('get_dom_list') ? get_dom_list('ru_ru','org_form_dom',true) : array();
 
 ?>
--- custom/application/Ext/Language/ru_ru.lang.ext.php
+++ custom/application/Ext/Language/ru_ru.lang.ext.php
@@ -10,7 +10,7 @@ $app_list_strings['resident_list'] = array(
     'rf' => 'РФ',
     'not_rf' => 'Не РФ',
 );
-$app_list_strings['org_form_dom']= get_dom_list('ru_ru','org_form_dom',true);
+$app_list_strings['org_form_dom'] = function_exists('get_dom_list') ? get_dom_list('ru_ru','org_form_dom',true) : array();
 
 
 ?>
