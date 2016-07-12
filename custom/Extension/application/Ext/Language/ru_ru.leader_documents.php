df2169b7b63757f85fd64fc5c4b4c8ade629b5e4
Leon Nikitin <nlv@lab321.ru>
2016-07-12 16:29:35 +0600

    Обновлены пакеты houses и leader_documents

---
+++ custom/Extension/application/Ext/Language/ru_ru.leader_documents.php
@@ -0,0 +1,17 @@
+<?php
+
+$app_list_strings['document_category_dom'] = [
+    '1' => 'Документы и отчетность',
+    '2' => 'Капитальный ремонт',
+    '3' => 'Общее имущество',
+    '4' => 'Медиа'
+];
+$app_list_strings['document_subcategory_dom'] = [];
+
+$app_list_strings['document_template_type_dom'] = [
+  '1' => 'Фото',
+  '2' => 'Договоры',
+  '3' => 'Протоколы',
+];
+  
+?>
