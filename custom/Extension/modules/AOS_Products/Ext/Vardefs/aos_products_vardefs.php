f3df7182fc72f9f001da2639215db41a70592682
Leon Nikitin <nlv@lab321.ru>
2016-08-05 13:45:54 +0600

    Добавлен пакет leader_aos_products

---
+++ custom/Extension/modules/AOS_Products/Ext/Vardefs/aos_products_vardefs.php
@@ -0,0 +1,17 @@
+<?php 
+
+$dictionary["AOS_Products"]["fields"]["measure"] = array (
+    'name' => 'measure',
+    'required' => true,
+    'vname' => 'LBL_MEASURE',
+    'type' => 'varchar',
+    'massupdate' => 1,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => 1,
+    'reportable' => true,
+    'len' => '255',
+);
