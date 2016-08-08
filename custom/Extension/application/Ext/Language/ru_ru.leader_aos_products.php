6e5820fc6e56b89bba74d1e5e33f3572d6e70d1b
Leon Nikitin <nlv@lab321.ru>
2016-08-08 11:33:31 +0600

    Обновлен пакет aos_leader_products (добавлен тип категории продуктов)

---
+++ custom/Extension/application/Ext/Language/ru_ru.leader_aos_products.php
@@ -0,0 +1,8 @@
+<?php
+
+$app_list_strings['aos_product_categories_dom'] = [
+    '' => '',
+    'PaidServices' => 'Платные услуги',
+];
+  
+?>
---
+++ custom/Extension/modules/AOS_Product_Categories/Ext/Vardefs/aos_product_categories_vardefs.php
@@ -0,0 +1,20 @@
+<?php 
+
+$dictionary["AOS_Product_Categories"]["fields"]["type"] = array (
+                'required' => '0',
+                'name' => 'type',
+                'vname' => 'LBL_TYPE',
+                'type' => 'enum',
+                'massupdate' => 0,
+                'default' => '',
+                'comments' => '',
+                'help' => '',
+                'importable' => 'true',
+                'duplicate_merge' => 'disabled',
+                'duplicate_merge_dom_value' => '0',
+                'audited' => 1,
+                'reportable' => true,
+                'len' => 100,
+                'options' => 'aos_product_categories_dom',
+                'studio' => 'visible',
+            );
