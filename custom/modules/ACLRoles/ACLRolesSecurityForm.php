ccb9d1ff94b0fb8df942167acbde754fad64855e
Евгений Первушин <pea@lab321.ru>
2016-08-24 14:10:33 +0600

    Роль для новых обращений

--- custom/modules/ACLRoles/ACLRolesSecurityForm.php
+++ custom/modules/ACLRoles/ACLRolesSecurityForm.php
@@ -11,7 +11,12 @@ class ACLRolesSecurityForm extends SecurityForm
     function setBean($bean)
     {
         parent::setBean($bean);
-        if($bean->id == 'default' || $bean->id == 'admin') {
+        if(in_array($bean->id, array(
+            'default',
+            'admin',
+            'case-front',
+        )))
+        {
             $this->setDefaultFieldsMode(SecurityForm::MODE_DEFAULT_DISABLED);
             $this->setEnabledFields(array('description'));
         }
